7491398906f82012874dd17fe8c0327544f72270
AllTheCodeWeCannotSee <183735517@qq.com>
2023-09-19 19:52:07 +0800

    chat-input-component & message-api

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {redirect} from "next/navigation";
+
 import {currentProfile} from "@/lib/current-profile";
 import {redirectToSignIn} from "@clerk/nextjs";
 import {db} from "@/lib/db";
-import {redirect} from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
+import {ChatInput} from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
     params: {
@@ -43,6 +45,16 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">Future Messages</div>
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     );
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,9 +1,8 @@
 import { Hash } from "lucide-react";
 
-import {MobileToggle} from "@/components/mobile-toggle";
-import {UserAvatar} from "@/components/user-avatar";
-import {SocketIndicator} from "@/components/socket-indicator";
-
+import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -20,8 +19,7 @@ export const ChatHeader = ({
                            }: ChatHeaderProps) => {
     return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-            <MobileToggle serverId={serverId}/>
-
+            <MobileToggle serverId={serverId} />
             {type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
             )}
@@ -31,11 +29,9 @@ export const ChatHeader = ({
                     className="h-8 w-8 md:h-8 md:w-8 mr-2"
                 />
             )}
-
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
-
             <div className="ml-auto flex items-center">
                 <SocketIndicator />
             </div>
