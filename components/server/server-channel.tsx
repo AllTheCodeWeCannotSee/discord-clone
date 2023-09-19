64b8c7e5fe7d994a44d45887366e7d0d413b3464
AllTheCodeWeCannotSee <183735517@qq.com>
2023-09-19 09:09:48 +0800

    channel id page

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { 
-  Channel, 
-  ChannelType, 
+import {
+  Channel,
+  ChannelType,
   MemberRole,
   Server
 } from "@prisma/client";
@@ -26,10 +26,10 @@ const iconMap = {
 }
 
 export const ServerChannel = ({
-  channel,
-  server,
-  role
-}: ServerChannelProps) => {
+                                channel,
+                                server,
+                                role
+                              }: ServerChannelProps) => {
   const { onOpen } = useModal();
   const params = useParams();
   const router = useRouter();
@@ -46,41 +46,41 @@ export const ServerChannel = ({
   }
 
   return (
-    <button
-      onClick={onClick}
-      className={cn(
-        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-        params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
-      )}
-    >
-      <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
-      <p className={cn(
-        "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-        params?.channelId === channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
-      )}>
-        {channel.name}
-      </p>
-      {channel.name !== "general" && role !== MemberRole.GUEST && (
-        <div className="ml-auto flex items-center gap-x-2">
-          <ActionTooltip label="Edit">
-            <Edit
-              onClick={(e) => onAction(e, "editChannel")}
-              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+      <button
+          onClick={onClick}
+          className={cn(
+              "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+              params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+          )}
+      >
+        <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+        <p className={cn(
+            "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+            params?.channelId === channel.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}>
+          {channel.name}
+        </p>
+        {channel.name !== "general" && role !== MemberRole.GUEST && (
+            <div className="ml-auto flex items-center gap-x-2">
+              <ActionTooltip label="Edit">
+                <Edit
+                    onClick={(e) => onAction(e, "editChannel")}
+                    className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+                />
+              </ActionTooltip>
+              <ActionTooltip label="Delete">
+                <Trash
+                    onClick={(e) => onAction(e, "deleteChannel")}
+                    className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+                />
+              </ActionTooltip>
+            </div>
+        )}
+        {channel.name === "general" && (
+            <Lock
+                className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400"
             />
-          </ActionTooltip>
-          <ActionTooltip label="Delete">
-            <Trash
-              onClick={(e) => onAction(e, "deleteChannel")}
-              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-            />
-          </ActionTooltip>
-        </div>
-      )}
-      {channel.name === "general" && (
-        <Lock
-          className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400"
-        />
-      )}
-    </button>
+        )}
+      </button>
   )
 }
